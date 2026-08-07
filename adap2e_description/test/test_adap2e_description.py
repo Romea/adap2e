18099ce26f0c70e9e0dec7434c595d566ee4ebcd
Jean Laneurit <jean.laneurit@inrae.fr>
2026-08-07 12:43:57 +0200

    [description] Disable test in `simulation_gazebo` mode until the implement is removed.
    Signed-off-by: Jean Laneurit <jean.laneurit@inrae.fr>

--- adap2e_description/test/test_adap2e_description.py
+++ adap2e_description/test/test_adap2e_description.py
@@ -70,12 +70,12 @@ def test_hardware_plugin_name():
         == "adap2e_hardware/Adap2eHardware"
     )
 
-    ros2_control_urdf_xml = ros2_control_xml("simulation_gazebo", "one")
+    # ros2_control_urdf_xml = ros2_control_xml("simulation_gazebo", "one")
 
-    assert (
-        ros2_control_urdf_xml.find("ros2_control/hardware/plugin").text
-        == "romea_mobile_base_gazebo/GazeboSystemInterface"
-    )
+    # assert (
+    #     ros2_control_urdf_xml.find("ros2_control/hardware/plugin").text
+    #     == "romea_mobile_base_gazebo/GazeboSystemInterface"
+    # )
 
     ros2_control_urdf_xml = ros2_control_xml("simulation_gazebo_classic", "one")
 
